6370787e864bcb30d11ee7169e6c3f000b2d9d83
John OConnell <johnathano619@gmail.com>
2019-05-18 10:11:03 -0400

    Make better alerts when the user does something silly

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,7 +1,3 @@
-//Script .js
-
-//Custom Functions
-
 function calculateTip() {
 	
 	//Store the data of the inputs
@@ -10,19 +6,22 @@ function calculateTip() {
 		numPeople = document.getElementById("totalPeople").value;
 	
 	//Quick validation (when folks do silly things)
-	if (billAmount === "0.00" || serviceQuality === 0) {
-		window.alert("Please enter some values to get this baby up and running!");
+	if (billAmount === "0.00" && serviceQuality == 0) {
+		window.alert('Hey! So, what are you even doing here?');
+	}
+	else if (billAmount === "0.00" && serviceQuality !== 0) {
+		window.alert("Woah! Get a load a Mr. FreeEats over here!");
+	} 
+	else if (billAmount >= "0.00" && serviceQuality == 0) {
+		window.alert('Come on, be honest. How was it?');
 		return; //This will prevent the function from continuting
 	}
-	
+
 	//Check to see if input is empty or >= to 1
 	if (numPeople === "" || numPeople <= 1) {
 		numPeople = 1; //If user enters nothing this ensures value is at least one
-		
 		document.getElementById("each").style.display = "none";
-		
 	} else {
-		
 		document.getElementById("each").style.display = "block";
 	}
 
@@ -44,11 +43,8 @@ function calculateTip() {
 }
 
 //Hide tip amount on load
-
 document.getElementById("totalTip").style.display = "none";
-
 document.getElementById("each").style.display = "none";
 
 //Clicking the button calls our custom function
-
 document.getElementById("calculate").onclick = function () { calculateTip(); };
